59be8973b7e9805bb0b1edbe977bd311218df760
ResearcherCleveS <sclevel3@alumni.jh.edu>
2025-09-16 17:00:17 -0400

    Update tfBSPDE_PINN.py

--- tfBSPDE_PINN.py
+++ tfBSPDE_PINN.py
@@ -231,6 +231,11 @@ for j, alpha in enumerate(np.array([0.1, 0.3, 0.7, 0.9])):
     yaxis_title='Time to maturity',
     zaxis_title='Option price'
     ))
+fig = make_subplots(
+rows=2, cols=2,
+specs=[[{'type': 'surface'}] * 2] * 2,
+subplot_titles=('alpha = 0.1', 'alpha = 0.3', 'alpha = 0.7', 'alpha = 0.9')
+)
 fig.add_trace(
     go.Surface(x=x_test, y=t_test, z=u_pred_lst[0], colorscale='Viridis', showscale=True,
     opacity=0.75),
